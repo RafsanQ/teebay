399c51271e741928324795359125bfabc9e8babc
Rafsan <rafsan.quayes@gmail.com>
2023-08-20 16:23:05 +0600

    added graphql api and it works

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const createError = require('http-errors');
 const morgan = require('morgan');
 require('dotenv').config();
 
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,6 +14,34 @@ app.use(morgan('dev'));
 app.get('/', async (req, res, next) => {
   res.send({ message: 'Awesome it works 🐻' });
 });
+
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
+      type RootQuery {
+        products: [String!]!
+
+      }
+
+      type RootMutation {
+        createProduct(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+  `),
+  rootValue: {
+    products: () => {
+      return ['random', 'String', 'Example 1', 'Product example 2'];
+    },
+    createProduct: (args) => {
+      const productName = args.name;
+      return productName;
+    }
+  },
+  graphiql: true
+}) )
 
 app.use('/api', require('./routes/api.route'));
 
