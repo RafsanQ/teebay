ad6ee5e8651538be6e8781e03cff40fef406c6a5
RafsanQ <rafsan.quayes@gmail.com>
2024-11-13 22:39:43 +0600

    fixed a few typos

--- server/app.js
+++ server/app.js
@@ -1,60 +1,65 @@
-import express from 'express';
-import createError from 'http-errors';
-import morgan from 'morgan';
+import express from "express";
+import createError from "http-errors";
+import morgan from "morgan";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { graphqlHTTP } from 'express-graphql';
+import { graphqlHTTP } from "express-graphql";
 
-import graphqlSchema from './graphql/schema/index.js'
-import graphqlResolvers from './graphql/resolvers/index.js'
+import graphqlSchema from "./graphql/schema/index.js";
+import graphqlResolvers from "./graphql/resolvers/index.js";
 
-import isAuthorised from './middleware/auth.js';
+import isAuthorized from "./middleware/auth.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', async (req, res, next) => {
-  res.send({ message: 'Awesome it works 🐻' });
+app.get("/", async (req, res, next) => {
+    res.send({ message: "Awesome it works 🐻" });
 });
 
 // CORS Configuration Middleware
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST', 'GET', 'OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST", "GET", "OPTIONS");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
 
-  if(req.method === 'OPTIONS'){
-    return res.sendStatus(200);
-  }
-  next();
-})
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 // Middleware to check token
-app.use(isAuthorised)
+app.use(isAuthorized);
 
-app.use('/graphql', graphqlHTTP({
-  schema: graphqlSchema,
-  rootValue: graphqlResolvers,
-  graphiql: true
-}))
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: graphqlSchema,
+        rootValue: graphqlResolvers,
+        graphiql: true,
+    })
+);
 
 // app.use('/api', require('./routes/api.route'));
 
-
 app.use((req, res, next) => {
-  next(createError.NotFound());
+    next(createError.NotFound());
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.send({
-    status: err.status || 500,
-    message: err.message,
-  });
+    res.status(err.status || 500);
+    res.send({
+        status: err.status || 500,
+        message: err.message,
+    });
 });
 
 const PORT = process.env.PORT || 3000;
